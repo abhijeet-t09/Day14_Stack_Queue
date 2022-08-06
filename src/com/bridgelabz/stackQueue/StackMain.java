3d55c8edf255fd377284ea7bbb068d769588516d
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-06 18:09:41 +0530

    Create Queue

--- src/com/bridgelabz/stackQueue/StackMain.java
+++ src/com/bridgelabz/stackQueue/StackMain.java
@@ -2,18 +2,24 @@ package com.bridgelabz.stackQueue;
 
 public class StackMain {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(70);
-        stack.push(30);
-        stack.push(56);
-        stack.print();
-        System.out.println();
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(70);
+//        stack.push(30);
+//        stack.push(56);
+//        stack.print();
+//        System.out.println();
+//
+//        while(!stack.isEmpty()){
+//            System.out.println(stack.peek());
+//            stack.pop();
+//            stack.print();
+//            System.out.println();
+//        }
 
-        while(!stack.isEmpty()){
-            System.out.println(stack.peek());
-            stack.pop();
-            stack.print();
-            System.out.println();
-        }
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+        queue.print();
     }
 }
